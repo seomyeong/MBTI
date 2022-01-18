e1ae890559123f902577d5129336111a9e63cd5e
seomyeong <arri123@naver.com>
2022-01-18 19:19:56 +0900

    서명 index 배너 효과

---
+++ MBTI/src/main/webapp/resources/js/index.js
@@ -0,0 +1,11 @@
+/* 각 mbti 캐릭터 랜덤 등장 */
+let num = 0;
+for (let i = 1; i <= 16; i++) {
+    $('.avatar' + i + '').css({ opacity: 0 })
+    num++;
+    let random = Math.floor(Math.random() * 2000) + 500;
+    for (let i = 0; i < 1; i++) {
+
+        $('.avatar' + num + '').delay(random).animate({ opacity: 1 }, 500)
+    }
+}
