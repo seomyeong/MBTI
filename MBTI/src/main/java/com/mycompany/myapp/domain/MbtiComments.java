93071d039c31d718a675dc2adfddc9bab4086b72
chung <chung@192.168.219.119>
2022-01-23 01:14:41 +0900

    지영 mbtiMatch

--- MBTI/src/main/java/com/mycompany/myapp/domain/MbtiComments.java
+++ MBTI/src/main/java/com/mycompany/myapp/domain/MbtiComments.java
@@ -12,17 +12,17 @@ public class MbtiComments {
 	private Member member;
 	private String type01;
 	private String type02;
-	private String comments;
+	private String comment;
 	private String reportingDate;
 	
 	public MbtiComments() {}
 	
-	public MbtiComments(Member member, String type01, String type02, String comments, String reportingDate) {
+	public MbtiComments(Member member, String type01, String type02, String comment, String reportingDate) {
 		super();
 		this.member = member;
 		this.type01 = type01;
 		this.type02 = type02;
-		this.comments = comments;
+		this.comment = comment;
 		this.reportingDate = reportingDate;
 	}
 	
